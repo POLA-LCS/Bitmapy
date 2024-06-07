527aee6f236c2d419af2b1febcd712720766fca2
POLA-LCS <zara.pilling18@gmail.com>
2024-06-07 12:46:29 -0300

    Change the canvas privacy and documentation

--- bitmapy/bitmap.py
+++ bitmapy/bitmap.py
@@ -3,32 +3,42 @@ from PIL import Image
 from .types import *
 
 class Bitmap:
+    """## Bitmap\n
+    (documentation in process...)"""
     def __init__(self, width: int, height: int, default_color: Color | None = (255, 255, 255)):
         self.width = width
         self.height = height
         self.default_color =  default_color
-        self.canvas = [[default_color] * width for _ in range(height)]
+        self.__canvas = [[default_color] * width for _ in range(height)]
+
+    @property
+    def canvas(self):
+        return self.__canvas
 
     def draw(self, *pixels: Pixel):
+        """Pixel format: `((r, g, b), (x, y))`"""
         for pix in pixels:
             color = pix[0]
             x, y = pix[1]
             if 1 <= x <= self.width and 1 <= y <= self.height:
-                self.canvas[y - 1][x - 1] = color
+                self.__canvas[y - 1][x - 1] = color
 
+    def erase(self, *positions: Coord):
+        """Draws in the `default_color` of the bitmap"""
+        for pos in positions:
+            self.draw((self.default_color, pos))
+    
     def draw_area(self, color: Color, from_pos: Coord, to_pos: Coord):
+        """Draws an area of `color`, the order of the `from_pos` and `to_pos` is switchable"""
         x1, y1 = from_pos
         x2, y2 = to_pos
         for y in range(min(y1, y2), max(y1, y2) + 1):
             for x in range(min(x1, x2), max(x1, x2) + 1):
                 if 1 <= x <= self.width and 1 <= y <= self.height:
-                    self.canvas[y-1][x-1] = color
+                    self.__canvas[y-1][x-1] = color
 
-    def erase(self, *positions: Coord):
-        for pos in positions:
-            self.draw((self.default_color, pos))
-        
     def draw_cross(self, color: Color, pos: Coord, size: int):
+        """Draws a pixel with four other pixels on the sides separated by `size`"""
         self.draw(color, pos)
         self.draw(color, (pos[0] + size, pos[1]))
         self.draw(color, (pos[0] - size, pos[1]))
@@ -47,7 +57,7 @@ class Bitmap:
         
         image_data = bytearray(4 * self.width * self.height)
         idx = 0
-        for row in reversed(self.canvas):
+        for row in reversed(self.__canvas):
             for color in row:
                 image_data[idx:idx + 4] = struct.pack('BBBB', color[2], color[1], color[0], 255) # ALPHA 100%
                 idx += 4
